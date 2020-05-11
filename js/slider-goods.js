c386788dd75ec338d588d29ab2788eb27d02881d
bedranyv <evgen.bedran@gmail.com>
2020-05-12 01:44:41 +0300

    Убрал скрывающую анимацию слайдов
    Из-за несовместимости с мобильными браузерами и Edge

--- js/slider-goods.js
+++ js/slider-goods.js
@@ -8,57 +8,57 @@ let buttonSlider3 = document.querySelector(".slider-button-3");
 
 
 
-function showSlider1_2() {
-	slider1.classList.remove("slider-show-first");
+// function showSlider1_2() {
+	// slider1.classList.remove("slider-show-first");
+	// slider1.classList.remove("slider-show");
+	// slider1.classList.remove("slider-anim-hide-class");
+// }
 
-	slider1.classList.remove("slider-show");
-	slider1.classList.remove("slider-anim-hide-class");
-}
+// function showSlider1_3() {
+	// slider1.classList.remove("slider-show-first");
+	// slider1.classList.remove("slider-show");
+	// slider1.classList.remove("slider-anim-hide-class");
+// }
 
-function showSlider1_3() {
-	slider1.classList.remove("slider-show-first");
+// function showSlider2_1() {
+	// slider2.classList.remove("slider-show");
+	// slider2.classList.remove("slider-anim-hide-class");	
+// }
 
-	slider1.classList.remove("slider-show");
-	slider1.classList.remove("slider-anim-hide-class");
-}
+// function showSlider2_3() {
+	// slider2.classList.remove("slider-show");
+	// slider2.classList.remove("slider-anim-hide-class");	
+// }
 
-function showSlider2_1() {
-	slider2.classList.remove("slider-show");
-	slider2.classList.remove("slider-anim-hide-class");	
-}
+// function showSlider3_1() {
+	// slider3.classList.remove("slider-show");
+	// slider3.classList.remove("slider-anim-hide-class");
+// }
 
-function showSlider2_3() {
-	slider2.classList.remove("slider-show");
-	slider2.classList.remove("slider-anim-hide-class");	
-}
-
-function showSlider3_1() {
-	slider3.classList.remove("slider-show");
-	slider3.classList.remove("slider-anim-hide-class");
-}
-
-function showSlider3_2() {
-	slider3.classList.remove("slider-show");
-	slider3.classList.remove("slider-anim-hide-class");
-}
+// function showSlider3_2() {
+	// slider3.classList.remove("slider-show");
+	// slider3.classList.remove("slider-anim-hide-class");
+// }
 
 buttonSlider1.addEventListener("click", function (evt) {
 	evt.preventDefault();
 
 	if (slider2.classList.contains('slider-show')) {
 
-		slider2.classList.add("slider-anim-hide-class");
+		// slider2.classList.add("slider-anim-hide-class");
+		slider2.classList.remove("slider-show");
 		slider1.classList.add("slider-show");
 		buttonSlider2.classList.remove("slider-button-current");
 		buttonSlider1.classList.add("slider-button-current");
-		setTimeout(showSlider2_1, 500);
+		// setTimeout(showSlider2_1, 500);
 	}
 	else {
-		slider3.classList.add("slider-anim-hide-class");
+		// slider3.classList.add("slider-anim-hide-class");
+		slider3.classList.remove("slider-show");
 		slider1.classList.add("slider-show");
 		buttonSlider3.classList.remove("slider-button-current");
 		buttonSlider1.classList.add("slider-button-current");
-		setTimeout(showSlider3_1, 500);
+		// setTimeout(showSlider3_1, 500);
 	}
 });
 
@@ -67,18 +67,21 @@ buttonSlider2.addEventListener("click", function (evt) {
 
 	if (slider3.classList.contains('slider-show')) {
 
-		slider3.classList.add("slider-anim-hide-class");
+		// slider3.classList.add("slider-anim-hide-class");
+		slider3.classList.remove("slider-show");
 		slider2.classList.add("slider-show");
 		buttonSlider3.classList.remove("slider-button-current");
 		buttonSlider2.classList.add("slider-button-current");
-		setTimeout(showSlider3_2, 500);
+		// setTimeout(showSlider3_2, 500);
 	}
 	else {
-		slider1.classList.add("slider-anim-hide-class");
+		// slider1.classList.add("slider-anim-hide-class");
+		slider1.classList.remove("slider-show-first");
+		slider1.classList.remove("slider-show");
 		slider2.classList.add("slider-show");
 		buttonSlider1.classList.remove("slider-button-current");
 		buttonSlider2.classList.add("slider-button-current");
-		setTimeout(showSlider1_2, 500);
+		// setTimeout(showSlider1_2, 500);
 	}
 });
 
@@ -87,17 +90,20 @@ buttonSlider3.addEventListener("click", function (evt) {
 
 	if (slider1.classList.contains('slider-show') || slider1.classList.contains('slider-show-first')) {
 
-		slider1.classList.add("slider-anim-hide-class");
+		// slider1.classList.add("slider-anim-hide-class");
+		slider1.classList.remove("slider-show-first");
+		slider1.classList.remove("slider-show");
 		slider3.classList.add("slider-show");
 		buttonSlider1.classList.remove("slider-button-current");
 		buttonSlider3.classList.add("slider-button-current");
-		setTimeout(showSlider1_3, 500);
+		// setTimeout(showSlider1_3, 500);
 	}
 	else {
-		slider2.classList.add("slider-anim-hide-class");
+		// slider2.classList.add("slider-anim-hide-class");
+		slider2.classList.remove("slider-show");
 		slider3.classList.add("slider-show");
 		buttonSlider2.classList.remove("slider-button-current");
 		buttonSlider3.classList.add("slider-button-current");
-		setTimeout(showSlider2_3, 500);
+		// setTimeout(showSlider2_3, 500);
 	}
 });
--- js/slider-services.js
+++ js/slider-services.js
@@ -8,64 +8,59 @@ let buttonService3 = document.querySelector(".services-button-3");
 
 
 
-function showServiceSlider1_2() {
-	sliderService1.classList.remove("services-slider-show-first");
+// function showServiceSlider1_2() {
+// 	sliderService1.classList.remove("services-slider-show-first");
+// 	sliderService1.classList.remove("services-slider-show");
+// 	sliderService1.classList.remove("services-slider-anim-hide-class");
+// }
 
-	sliderService1.classList.remove("services-slider-show");
-	sliderService1.classList.remove("services-slider-anim-hide-class");
-}
+// function showServiceSlider1_3() {
+// 	sliderService1.classList.remove("services-slider-show-first");
+// 	sliderService1.classList.remove("services-slider-show");
+// 	sliderService1.classList.remove("services-slider-anim-hide-class");
+// }
 
-function showServiceSlider1_3() {
-	sliderService1.classList.remove("services-slider-show-first");
+// function showServiceSlider2_1() {
+// 	sliderService2.classList.remove("services-slider-show");
+// 	sliderService2.classList.remove("services-slider-anim-hide-class");	
+// }
 
-	sliderService1.classList.remove("services-slider-show");
-	sliderService1.classList.remove("services-slider-anim-hide-class");
-}
+// function showServiceSlider2_3() {
+// 	sliderService2.classList.remove("services-slider-show");
+// 	sliderService2.classList.remove("services-slider-anim-hide-class");	
+// }
 
-function showServiceSlider2_1() {
-	sliderService2.classList.remove("services-slider-show");
-	sliderService2.classList.remove("services-slider-anim-hide-class");	
-}
+// function showServiceSlider3_1() {
+// 	sliderService3.classList.remove("services-slider-show");
+// 	sliderService3.classList.remove("services-slider-anim-hide-class");
+// }
 
-function showServiceSlider2_3() {
-	sliderService2.classList.remove("services-slider-show");
-	sliderService2.classList.remove("services-slider-anim-hide-class");	
-}
-
-function showServiceSlider3_1() {
-	sliderService3.classList.remove("services-slider-show");
-	sliderService3.classList.remove("services-slider-anim-hide-class");
-}
-
-function showServiceSlider3_2() {
-	sliderService3.classList.remove("services-slider-show");
-	sliderService3.classList.remove("services-slider-anim-hide-class");
-}
+// function showServiceSlider3_2() {
+// 	sliderService3.classList.remove("services-slider-show");
+// 	sliderService3.classList.remove("services-slider-anim-hide-class");
+// }
 
 buttonService1.addEventListener("click", function (evt) {
 	evt.preventDefault();
 
 	if (sliderService2.classList.contains('services-slider-show')) {
 
-		sliderService2.classList.add("services-slider-anim-hide-class");
+		// sliderService2.classList.add("services-slider-anim-hide-class");
+		sliderService2.classList.remove("services-slider-show");
 		sliderService1.classList.add("services-slider-show");
 
 		buttonService2.classList.remove("services-button-active");
-
 		buttonService1.classList.add("services-button-active");
-
 
 		setTimeout(showServiceSlider2_1, 500);
 	}
 	else {
-		sliderService3.classList.add("services-slider-anim-hide-class");
+		// sliderService3.classList.add("services-slider-anim-hide-class");
+		sliderService3.classList.remove("services-slider-show");
 		sliderService1.classList.add("services-slider-show");
 
-
 		buttonService3.classList.remove("services-button-active");
-
 		buttonService1.classList.add("services-button-active");
-
 
 		setTimeout(showServiceSlider3_1, 500);
 	}
@@ -76,26 +71,23 @@ buttonService2.addEventListener("click", function (evt) {
 
 	if (sliderService3.classList.contains('services-slider-show')) {
 
-		sliderService3.classList.add("services-slider-anim-hide-class");
+		// sliderService3.classList.add("services-slider-anim-hide-class");
+		sliderService3.classList.remove("services-slider-show");
 		sliderService2.classList.add("services-slider-show");
 
-
 		buttonService3.classList.remove("services-button-active");
-
 		buttonService2.classList.add("services-button-active");
-
 
 		setTimeout(showServiceSlider3_2, 500);
 	}
 	else {
-		sliderService1.classList.add("services-slider-anim-hide-class");
+		// sliderService1.classList.add("services-slider-anim-hide-class");
+		sliderService1.classList.remove("services-slider-show-first");
+		sliderService1.classList.remove("services-slider-show");
 		sliderService2.classList.add("services-slider-show");
 
 		buttonService1.classList.remove("services-button-active");
-
-
 		buttonService2.classList.add("services-button-active");
-
 
 		setTimeout(showServiceSlider1_2, 500);
 	}
@@ -106,26 +98,23 @@ buttonService3.addEventListener("click", function (evt) {
 
 	if (sliderService1.classList.contains('services-slider-show') || sliderService1.classList.contains('services-slider-show-first')) {
 
-		sliderService1.classList.add("services-slider-anim-hide-class");
+		// sliderService1.classList.add("services-slider-anim-hide-class");
+		sliderService1.classList.remove("services-slider-show-first");
+		sliderService1.classList.remove("services-slider-show");
 		sliderService3.classList.add("services-slider-show");
 
 		buttonService1.classList.remove("services-button-active");
-
-
 		buttonService3.classList.add("services-button-active");
-
 
 		setTimeout(showServiceSlider1_3, 500);
 	}
 	else {
-		sliderService2.classList.add("services-slider-anim-hide-class");
+		// sliderService2.classList.add("services-slider-anim-hide-class");
+		sliderService2.classList.remove("services-slider-show");
 		sliderService3.classList.add("services-slider-show");
 
-
 		buttonService2.classList.remove("services-button-active");
-
 		buttonService3.classList.add("services-button-active");
-
 
 		setTimeout(showServiceSlider2_3, 500);
 	}
